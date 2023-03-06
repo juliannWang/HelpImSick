6784edee434ab88c4550bf7f3e19d6f513aeddfd
Katie Nethercot <2666751n@student.gla.ac.uk>
2023-03-06 13:13:55 +0000

    probs wrong-logout and index view partially done

--- HelpImSick/urls.py
+++ HelpImSick/urls.py
@@ -13,9 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from rango import views
+
+app_name = 'HelpImSick'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('index/',views.index, name='index'),
+    path('login/', views.login, name='index'), #view done
+    path('logout/', views.user_logout, name='logout'),   #view done
+    path('createAccount/', views.createAccount, name='createAccount'), #view done
+    path('index/settings/', views.settings, name='settings'),
+    path('index/post/<slug:post_id_slug>', views.post name='post'),
+    path('index/search/', views.search, name='search'),
+    path('index/search/results', views.searchResults, name='searchResults'),
+    path('index/createPost/', views.createPost, name='createPost'),   #view done
+    path('index/myPosts/', views.myPosts, name='myPosts'),   #view done
+    path('index/nearestGP', views.nearestGp, name='nearestGP'),
+    
 ]
--- ImSick/views.py
+++ ImSick/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from ImSick.models import UserAccount
+from ImSick.models import Post
+from ImSick.models import Comment
+from ImSick.forms import UserForm
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
@@ -9,4 +13,19 @@ from django.contrib.auth.decorators import login_required
 def user_logout(request):
     logout(request)
 
-    return redirect(reverse('HelpImSick:index'))
+    return redirect(reverse('HelpImSick:homepage'))
+
+
+def index(request):
+    post_list=Post.objects
+    comments=Comment.objects
+
+    context_dict = {}
+    context_dict['posts']=post_list
+    context_dict['account']=UserAccount
+    context_dict['comments']=comments
+
+     
+    response = render(request, 'rango/index.html', context=context_dict)
+    
+    return response
