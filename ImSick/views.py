9d8832f5d214349f6f0421204a5033ea42439e89
Katie Nethercot <2666751n@student.gla.ac.uk>
2023-03-06 12:37:21 +0000

    logout completed

--- ImSick/views.py
+++ ImSick/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def user_logout(request):
+    logout(request)
+
+    return redirect(reverse('HelpImSick:index'))
